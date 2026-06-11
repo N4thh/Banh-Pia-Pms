8803cf41bebe99e854df0125c7901c124ac39a29
N4thh <nhatahh2003@gmail.com>
2026-06-11 21:27:35 +0900

    fix: update redis password

--- backend/src/redis/redis.module.ts
+++ backend/src/redis/redis.module.ts
@@ -12,6 +12,7 @@ import { REDIS_CLIENT } from './redis.constants';
         const redis = new Redis({
           host: process.env.REDIS_HOST || 'localhost',
           port: Number(process.env.REDIS_PORT) || 6379,
+          password: process.env.REDIS_PASSWORD || undefined,
           retryStrategy: (times) => Math.min(times * 50, 2000),
           maxRetriesPerRequest: null,
         });
